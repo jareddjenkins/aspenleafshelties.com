b8364d1972b967de1bd4cb8e14f3fd629214c105
Jared Jenkins <jareddjenkins@gmail.com>
2019-10-08 10:23:55 -0600

    mend

---
+++ src/app/enterdogs/editpages/pageListItem.ts
@@ -0,0 +1,9 @@
+import { Pages } from '../../pages';
+import { Dog } from 'src/app/dog';
+
+export class PageListItem {
+    dog: Dog
+    sortId: number
+    pageName: string
+    dogsId: number
+}
