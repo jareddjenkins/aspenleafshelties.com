489e26e4e363595a8ae5894a66c6f23af435731b
Jared Jenkins <jareddjenkins@gmail.com>
2020-04-26 16:54:51 -0600

    fix about

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import { DogDetailComponent } from './dog-detail/dog-detail.component';
 import { ListdogsComponent } from './enterdogs/listdogs/listdogs.component'
 import { EditdogComponent } from './enterdogs/editdog/editdog.component';
 import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
 import { EnterdogsComponent } from './enterdogs/enterdogs.component'
 import { ResourcesComponent } from './resources/resources.component'
 import { EditpagesComponent } from './enterdogs/editpages/editpages.component';
@@ -20,6 +21,7 @@ const routes: Routes = [
   { path: 'girls', component: GirlsComponent },
   { path: 'available', component: AvailableComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'about', component: AboutComponent },
   { path: 'resources', component: ResourcesComponent },
   { path: 'detail/:id', component: DogDetailComponent },
   {
