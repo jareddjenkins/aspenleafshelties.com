7b7144b8361ed8f525ca752fb7a55151734cdb58
Jared Jenkins <jareddjenkins@gmail.com>
2023-06-09 10:22:37 -0600

    @angular/material migration - migration-v15
    Updates the Angular Material to v15

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,11 +24,11 @@ import { EnterdogsComponent } from './enterdogs/enterdogs.component';
 import { ResourcesComponent } from './resources/resources.component';
 import { ImageCropperModule } from 'ngx-image-cropper';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, } from '@angular/material/button'
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatInputModule } from '@angular/material/input';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatFormFieldModule, } from '@angular/material/form-field';
+import { MatLegacyButtonModule as MatButtonModule, } from '@angular/material/legacy-button'
+import { MatLegacyCheckboxModule as MatCheckboxModule } from '@angular/material/legacy-checkbox';
+import { MatLegacyInputModule as MatInputModule } from '@angular/material/legacy-input';
+import { MatLegacyAutocompleteModule as MatAutocompleteModule } from '@angular/material/legacy-autocomplete';
+import { MatLegacyFormFieldModule as MatFormFieldModule, } from '@angular/material/legacy-form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { nameContainsPipe } from './namesearch.pipes';
 import { EditpagesComponent } from './enterdogs/editpages/editpages.component';
