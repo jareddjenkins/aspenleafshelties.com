c23a5622e2a11b37c1ac4053ffee9000f9aa606c
Jared Jenkins <jaredjenkins@Jareds-MacBook-Air.local>
2026-04-11 13:12:40 -0600

    good them saved. Working on layout and pattern to apply theme

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  imports: [RouterLink],
+  imports: [MatButtonModule, MatCardModule, RouterLink],
   standalone: true,
 })
 export class HomeComponent {}
--- src/app/topnav/topnav.component.ts
+++ src/app/topnav/topnav.component.ts
@@ -4,6 +4,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-topnav',
@@ -12,10 +14,12 @@ import { MatSidenavModule } from '@angular/material/sidenav';
   imports: [
     RouterLink,
     RouterLinkActive,
+    MatToolbarModule,
     MatButtonModule,
     MatDividerModule,
     MatIconModule,
     MatSidenavModule,
+    MatMenuModule,
   ],
   standalone: true,
 })
